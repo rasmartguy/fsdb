ecf0ac1bf2285ca21ccc31e8cc6c24d5a1db49f9
Boris Dergachov <42623189+froexilize@users.noreply.github.com>
2019-03-27 17:18:44 +0300

    Added appveyor build rules with msvc (#6)

--- README.rst
+++ README.rst
@@ -3,6 +3,10 @@ FSDB
 
 .. image:: https://travis-ci.com/froexilize/fsdb.svg?branch=master
 	:target: https://travis-ci.com/froexilize/fsdb
+.. image:: https://ci.appveyor.com/api/projects/status/4thtertrite0tgnf/branch/master?svg=true
+	:target: https://ci.appveyor.com/project/froexilize/fsdb
+.. image:: https://img.shields.io/badge/License-GPLv3-blue.svg
+	:target: LICENSE
 
 Abstract
 --------
@@ -14,7 +18,7 @@ Social
 
 Follow us on |twitter_image|_ |facebook_image|_
 
-.. |twitter_image| image:: http:///i.imgur.com/tXSoThF.png
+.. |twitter_image| image:: http://i.imgur.com/tXSoThF.png
 .. _twitter_image: http://www.twitter.com/rasmart_io
 .. |facebook_image| image:: http://i.imgur.com/P3YfQoD.png
 .. _facebook_image: http://www.facebook.com/rasmartblockchain
--- cmake/External_GTest.cmake
+++ cmake/External_GTest.cmake
@@ -1,12 +1,14 @@
 include(ExternalProject)
 
+set (gtest_force_shared_crt ON CACHE BOOL "Always use msvcrt.dll" FORCE)
+
 ExternalProject_Add(gtest_ext
 		GIT_REPOSITORY https://github.com/google/googletest.git
 		BINARY_DIR "${CMAKE_BINARY_DIR}/third-party/gmock-build"
 		SOURCE_DIR "${CMAKE_BINARY_DIR}/third-party/gmock-src"
 		INSTALL_DIR "${CMAKE_BINARY_DIR}/third-party/gtest-build"
 		CMAKE_ARGS "${GTEST_CMAKE_ARGS}"
-		"-DCMAKE_BUILD_TYPE=Release"
+		"-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%"
 		INSTALL_COMMAND ""
 		UPDATE_COMMAND ""
 		)
@@ -22,7 +24,7 @@ find_package(Threads REQUIRED)
 
 function(cxx_test name sources)
 	add_executable(${name} ${sources})
-	target_link_libraries(${name} ${ARGN} gtest ${CMAKE_THREAD_LIBS_INIT})
+	target_link_libraries(${name} ${ARGN} ${GTEST_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
 	set_property(TARGET ${name} APPEND PROPERTY INCLUDE_DIRECTORIES
 			${GTEST_INCLUDE_DIRS}
 			)
